4ce8f98d15e148794328a39fcf6c2d70a78abc98
Pavithra-Agraharam <53110141+Dark-AQwarior@users.noreply.github.com>
2023-11-27 01:34:27 -0600

    String to num to string

--- 2023/27th Nov/Trailing Zeros.py
+++ 2023/27th Nov/Trailing Zeros.py
@@ -4,3 +4,10 @@ class Solution:
     def removeTrailingZeros(self, num: str) -> str:
         return num.rstrip('0')
 # rstrip is used remove suffix character occurences of a string
+
+# Another code
+# converting string to integer so that 0's at end are gone, returning string again
+class Solution:
+    def removeTrailingZeros(self, num: str) -> str:
+        s = int(num[::-1])
+        return str(s)[::-1]
