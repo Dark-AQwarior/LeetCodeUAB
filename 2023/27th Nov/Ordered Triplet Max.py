52f058788b255cde8990a4b5d9accfd4eeb1b6c0
Pavithra-Agraharam <53110141+Dark-AQwarior@users.noreply.github.com>
2023-11-27 00:58:45 -0600

    Create Ordered Triplet Max.py

---
+++ 2023/27th Nov/Ordered Triplet Max.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description/
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        m = 0
+        if len(nums) == 3:
+            m = max(m, (nums[0] - nums[1]) * nums[2])
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    m = max(m, (nums[i] - nums[j]) * nums[k])
+
+        if m > 0:
+            return m
+        else:
+            return 0
